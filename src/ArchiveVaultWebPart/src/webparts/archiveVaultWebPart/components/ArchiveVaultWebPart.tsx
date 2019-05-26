ac2a8d703594176851e79a6e38c1d6a7731a34e2
Radi A <radi.a@live.com.au>
2019-05-26 14:34:07 +0300

    Improved the document style and table styles

--- src/ArchiveVaultWebPart/src/webparts/archiveVaultWebPart/components/ArchiveVaultWebPart.tsx
+++ src/ArchiveVaultWebPart/src/webparts/archiveVaultWebPart/components/ArchiveVaultWebPart.tsx
@@ -1,17 +1,19 @@
-import * as React from 'react';
-import styles from './ArchiveVaultWebPart.module.scss';
-import { IArchiveVaultWebPartProps } from './IArchiveVaultWebPartProps';
-import { escape } from '@microsoft/sp-lodash-subset';
-import { HttpClient, HttpClientResponse } from '@microsoft/sp-http';
+import * as React from "react";
+import styles from "./ArchiveVaultWebPart.module.scss";
+import { IArchiveVaultWebPartProps } from "./IArchiveVaultWebPartProps";
+import { escape } from "@microsoft/sp-lodash-subset";
+import { HttpClient, HttpClientResponse } from "@microsoft/sp-http";
 
-export default class ArchiveVaultWebPart extends React.Component<IArchiveVaultWebPartProps, any> {
+export default class ArchiveVaultWebPart extends React.Component<
+  IArchiveVaultWebPartProps,
+  any
+> {
   constructor(props: any) {
     super(props);
 
     this.state = {
       documents: []
     };
-
   }
 
   protected bindTable(data) {
@@ -19,33 +21,33 @@ export default class ArchiveVaultWebPart extends React.Component<IArchiveVaultWe
     data.map(document => {
       allElements.push(
         <tr>
-          <td><a href={document.SpFilePath}>{document.FileName}</a></td>
+          <td>
+            <a href={document.SpFilePath}>{document.FileName}</a>
+          </td>
           <td>{document.ConfidentialityLevel}</td>
           <td>{document.RetentionPeriod} years</td>
-        </tr>)
+        </tr>
+      );
     });
     return allElements;
   }
 
   public render(): React.ReactElement<IArchiveVaultWebPartProps> {
-
     return (
       <div className={styles.archiveVaultWebPart}>
         <div className={styles.container}>
           <div className={styles.row}>
             <div className={styles.column}>
               <span className={styles.title}>Archive Vault</span>
-              <table className="coolTable">
-                <thead className="tableStyle-thead">
-                  <td>File Name</td>
-                  <td>Confidentiality Level</td>
-                  <td>Retention Period</td>
+              <table className={styles.archiveTable}>
+                <thead className={styles.tableStylethead}>
+                  <tr>
+                    <td>File Name</td>
+                    <td>Confidentiality Level</td>
+                    <td>Retention Period</td>
+                  </tr>
                 </thead>
-                <tbody>
-                  {
-                    this.bindTable(this.state.documents)
-                  }
-                </tbody>
+                <tbody>{this.bindTable(this.state.documents)}</tbody>
               </table>
             </div>
           </div>
@@ -55,25 +57,19 @@ export default class ArchiveVaultWebPart extends React.Component<IArchiveVaultWe
   }
 
   public componentDidMount(): void {
-    var functionUrl = "https://archive-vault.azurewebsites.net/api/GetArchiveVaultDocuments?code=LDhSQkGDY9OBim1KYqDaZ9SQrs6J9eIYjDiWna/ISqgFBFJT8sz1Qg==";
+    var functionUrl =
+      "https://archive-vault.azurewebsites.net/api/GetArchiveVaultDocuments?code=LDhSQkGDY9OBim1KYqDaZ9SQrs6J9eIYjDiWna/ISqgFBFJT8sz1Qg==";
 
-    this.props.httpClient.get(functionUrl, HttpClient.configurations.v1)
+    this.props.httpClient
+      .get(functionUrl, HttpClient.configurations.v1)
       .then((response: HttpClientResponse) => {
         if (response.ok) {
-
-
           response.json().then(data => {
             this.setState({
-              documents: data,
+              documents: data
             });
             console.log(data);
-          }
-          );
-
-
-
-
-
+          });
         } else {
           Promise.resolve(null);
         }
